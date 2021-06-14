bab56574d889c5db129ec097705ff17396bd3b1e
Ivan Chavez <joseivan.chavezglz@gmail.com>
2021-06-14 01:31:12 -0500

    Se agrega funcionalidad de web sockets

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ import express from 'express'
 import dotenv  from 'dotenv'
 import bodyParser from 'body-parser';
 
+//Socket imports 
+
+import { createServer } from "http";
+import { Server } from "socket.io";
 //dotenv config
 dotenv.config()
 
@@ -18,6 +22,11 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var messages = [{ 
+    text: "Bienvenidos a la sala de chat del gimnasio", 
+    author: "~Entrenador" 
+  }]; 
 
 //Creating API 
 app.use('/api/clients', clientsRoutes)
@@ -30,6 +39,20 @@ app.use('/api/progreso', progresoRoutes)
 app.use('/api/tp', tiempoProgresoRoutes)
 
 const PORT = process.env.PORT || 5000
+
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on("connection", function(socket){
+    console.log('Alguien se ha conectado al chat con Sockets');   
+    socket.emit('messages', messages); 
+    socket.on('new-message', function(data) { 
+      messages.push(data); 
+      io.sockets.emit('messages', messages); 
+    }); 
+});
+
+httpServer.listen(3000,console.log('Corriendo servidor para websockets en el puerto 3000'));
 
 //Express js listen method to run project on http://localhost:5000
 app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
